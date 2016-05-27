0d065c50433ff496b98973d21dd514e49780e968
alpa1072 <alpa1072@naver.com>
2016-05-27 20:16:44 +0900

    Project001_grassTerrein

---
+++ Unity_Project/Android_Export/New Unity Project/src/com/lwj_unity/test_unity/UnityPlayerProxyActivity.java
@@ -0,0 +1,24 @@
+package com.lwj_unity.test_unity;
+
+import com.unity3d.player.*;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * @deprecated Use UnityPlayerActivity instead.
+ */
+public class UnityPlayerProxyActivity extends Activity
+{
+	protected void onCreate (Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		Intent intent = new Intent(this, com.lwj_unity.test_unity.UnityPlayerActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		Bundle extras = getIntent().getExtras();
+		if (extras != null)
+			intent.putExtras(extras);
+		startActivity(intent);
+	}
+}
