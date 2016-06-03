6888872cf9225ce268935ef67cd38a892b272601
alpa1072 <alpa1072@naver.com>
2016-06-03 22:07:29 +0900

    2nd_modified

--- Unity_Project/New Unity Project/Assets/Scripts/PlayerController.cs
+++ Unity_Project/New Unity Project/Assets/Scripts/PlayerController.cs
@@ -3,64 +3,55 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-	
-	//Vector2[] touchPos = new Vector2[5];
+
+    //Vector2[] touchPos = new Vector2[5];
 
     public float speed;
     private Rigidbody rb;
-	private int width;
-	private int height;
+    private int width;
+    private int height;
 
-	Vector3 rayDir;
-	public GameObject player;
 
     void Start()
     {
-		rb = GetComponent<Rigidbody>();
-		width = Screen.width;
-		height = Screen.height;
+        rb = GetComponent<Rigidbody>();
+        width = Screen.width;
+        height = Screen.height;
     }
 
-    void FixedUpdate()
+    void Update()
     {
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-		//////////////////////////////////////////////////////
-		int cnt = Input.touchCount;
+        //////////////////////////////////////////////////////
+        int cnt = Input.touchCount;
 
-		if (cnt == 1) {
-			Touch touch = Input.GetTouch (0);
-			Vector2 pos = touch.position;
+        if (cnt == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 pos = touch.position;
 
-			Ray ray = Camera.main.ScreenPointToRay (pos);
-			RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(pos);
+            RaycastHit hitInfo;
 
-			if (Physics.Raycast (ray, out hitInfo, 10000f)) {
-				float x = (hitInfo.point - player.transform.position).normalized.x;
-				float z = (hitInfo.point - player.transform.position).normalized.z;
-				moveHorizontal = x;
-				moveVertical = z;
-			}
-
-			/*if (touch.phase == TouchPhase.Began) {
-				Debug.Log (" start ");
-			}
-			else if (touch.phase == TouchPhase.Ended){
-				Debug.Log (" end ");
-				Ray ray = Camera.main.ScreenPointToRay(pos);
-				rayDir = ray.direction;
-				moveHorizontal = rayDir.x;
-				moveVertical = rayDir.z;//(pos.y)/height;
-			}
-			else if (touch.phase == TouchPhase.Moved){
-				Debug.Log (" moving ");
-			}*/
-		}
-		//////////////////////////////////////////////////////
+            if (Physics.Raycast(ray, out hitInfo, 10000f))
+            {
+                Vector3 vec = hitInfo.point - rb.transform.position;
+                float x = vec.normalized.x;
+                float z = vec.normalized.z;
+                moveHorizontal = x;
+                moveVertical = z;
+                float test = 1.0f;
+                if (z < 0.0f)
+                    test = -1.0f;
+                rb.transform.rotation = Quaternion.Euler(0.0f, Mathf.Acos(-x) * 180.0f * test / Mathf.PI, 0.0f);
+                rb.freezeRotation = true;
+            }
+        }
+        //////////////////////////////////////////////////////
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-		rb.AddForce(movement * speed, ForceMode.Acceleration);
-
+        rb.AddForce(movement * speed, ForceMode.VelocityChange);
     }
 }
