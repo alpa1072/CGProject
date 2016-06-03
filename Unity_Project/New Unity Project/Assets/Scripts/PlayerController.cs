b594628fa0c985040aae205b1af9e9a90f4423ed
alpa1072 <alpa1072@naver.com>
2016-06-03 16:32:56 +0900

    160603_1st

--- Unity_Project/New Unity Project/Assets/Scripts/PlayerController.cs
+++ Unity_Project/New Unity Project/Assets/Scripts/PlayerController.cs
@@ -3,23 +3,52 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+	
+	//Vector2[] touchPos = new Vector2[5];
 
     public float speed;
     private Rigidbody rb;
+	private int width;
+	private int height;
+
+	Vector3 rayDir;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+		rb = GetComponent<Rigidbody>();
+		width = Screen.width;
+		height = Screen.height;
     }
 
     void FixedUpdate()
     {
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+		//////////////////////////////////////////////////////
+		int cnt = Input.touchCount;
 
+		for (int i = 0; i < cnt; i++) {
+			Touch touch = Input.GetTouch (i);
+			Vector2 pos = touch.position;
+			if (touch.phase == TouchPhase.Began) {
+				Debug.Log (" start ");
+			}
+			else if (touch.phase == TouchPhase.Ended){
+				Debug.Log (" end ");
+				Ray ray = Camera.main.ScreenPointToRay(pos);
+				rayDir = ray.direction;
+				moveHorizontal = rayDir.x;
+				moveVertical = rayDir.z;//(pos.y)/height;
+			}
+			else if (touch.phase == TouchPhase.Moved){
+				Debug.Log (" moving ");
+			}
+		}
+		//////////////////////////////////////////////////////
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+		rb.AddForce(movement * speed, ForceMode.Acceleration);
 
     }
 }
